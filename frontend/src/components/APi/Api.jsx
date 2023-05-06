bb64d10be4f76f5ec55174f372aa49703f280019
ImeneRdn <imene.ramdan@live.fr>
2023-05-06 15:42:55 +0200

    recup modif

--- frontend/src/components/APi/Api.jsx
+++ frontend/src/components/APi/Api.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./Api.scss";
 
 function Api() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState("");
 
   useEffect(() => {
     fetch(
